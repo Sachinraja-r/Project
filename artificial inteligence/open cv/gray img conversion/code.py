c5f5a034fa529f12f6af0ea5613d68ca69c5b981
Sachin raja. R <sachinempire20@gmail.com>
2026-06-23 23:20:26 +0530

    Delete artificial inteligence/open cv directory

--- artificial inteligence/open cv/gray img conversion/code.py
+++
@@ -1,6 +0,0 @@
-import cv2
-img=cv2.imread("google.png")
-grayimg=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-cv2.imshow("original",img)
-cv2.imshow("grayimgcvt",grayimg)
-cv2.imwrite("grayimage.png",grayimg)
--- artificial inteligence/open cv/img resize/image resize.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import imutils
-img=cv2.imread("google.png")
-resize=imutils.resize(img,width=200)
-cv2.imshow("orgiginal",img)
-cv2.imshow("resize",resize)
-cv2.imwrite("resize img.png",resize)
